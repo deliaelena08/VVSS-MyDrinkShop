2f759d38ed8296451b6384f865134eb43ab3668b
deliaelena08 <deliatapuc@yahoo.com>
2026-04-14 14:31:44 +0300

    add homework 3 docs & refactor the receipt generator for the required complexity

--- src/main/java/drinkshop/receipt/ReceiptGenerator.java
+++ src/main/java/drinkshop/receipt/ReceiptGenerator.java
@@ -3,18 +3,48 @@ package drinkshop.receipt;
 import drinkshop.domain.Order;
 import drinkshop.domain.OrderItem;
 import drinkshop.domain.Product;
-
 import java.util.List;
 
 public class ReceiptGenerator {
-    public static String generate(Order o, List<Product> products) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("===== BON FISCAL =====\n").append("Comanda #").append(o.getId()).append("\n");
-        for (OrderItem i : o.getItems()) {
-            Product p = products.stream().filter((p1)->i.getProduct().getId()==p1.getId()).toList().get(0);
-            sb.append(p.getNume()+": ").append(p.getPret()).append(" x ").append(i.getQuantity()).append(" = ").append(i.getTotal()).append(" RON\n");
+
+    private static Product findProductById(List<Product> products, int id) {
+        for (Product p : products) {
+            if (p.getId() == id) {
+                return p;
+            }
         }
-        sb.append("---------------------\nTOTAL: ").append(o.getTotalPrice()).append(" RON\n=====================\n");
+        return null;
+    }
+
+    public static String generate(Order o, List<Product> products) {
+        if (o == null || products == null) {
+            return "Eroare: Date invalide!";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("===== BON FISCAL =====\n");
+        double totalCalculator = 0.0;
+
+        for (OrderItem i : o.getItems()) {
+            Product p = findProductById(products, i.getProduct().getId());
+
+            if (p != null) {
+                double lineTotal = p.getPret() * i.getQuantity();
+
+                if (i.getQuantity() > 5) {
+                    lineTotal = lineTotal * 0.9;
+                }
+
+                sb.append(p.getNume()).append(": ").append(lineTotal).append(" RON\n");
+                totalCalculator += lineTotal;
+            }
+        }
+
+        if (totalCalculator > 500) {
+            totalCalculator -= 50;
+        }
+
+        sb.append("---------------------\nTOTAL: ").append(totalCalculator).append(" RON\n");
         return sb.toString();
     }
 }
